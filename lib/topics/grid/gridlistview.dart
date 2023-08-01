5492cab1abd96085e2b5460423d7668ad15912bb
Nayeem Shiddiki Abir <abircoxsbazar@gmail.com>
2023-08-01 23:23:14 +0600

    updated grid view codebase

--- lib/topics/grid/gridlistview.dart
+++ lib/topics/grid/gridlistview.dart
@@ -6,24 +6,30 @@ class MyGridViewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Grid list"), centerTitle: true),
+      appBar: AppBar(title: const Text("Grid list"), centerTitle: true),
       body: GridView.builder(
           itemCount: 8,
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, crossAxisSpacing: 5, mainAxisSpacing: 5),
+              crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 10),
           itemBuilder: (context, index) {
             return Column(
               children: [
+                const SizedBox(height: 10, width: 10),
                 Container(
                   width: double.infinity,
-                  height: 150,
-                  decoration: const BoxDecoration(
+                  height: 170,
+                  decoration: BoxDecoration(
                       color: Colors.green,
-                      image: DecorationImage(
+                      borderRadius: BorderRadius.circular(20),
+                      image: const DecorationImage(
                           image: AssetImage("images/user.png"),
                           fit: BoxFit.cover)),
                 ),
-                Text("Abir")
+                const Text(
+                  "Nayeem Shiddiki Abir",
+                  textAlign: TextAlign.start,
+                  style: TextStyle(fontSize: 18)
+                )
               ],
             );
           }),
