a040a1048d30e0d462996370721b75be80e44c0d
Nayeem Shiddiki Abir <abircoxsbazar@gmail.com>
2023-08-14 17:39:56 +0600

    :hammer: debug badge removed

--- lib/httpreq/httpreg.dart
+++ lib/httpreq/httpreg.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.blue),
-      debugShowCheckedModeBanner: fa,
       darkTheme: ThemeData(primaryColor: Colors.grey),
       home: const MyScaffoldWidgets(),
     );
