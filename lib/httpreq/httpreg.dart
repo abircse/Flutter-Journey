e95bdf1dc0b76320ef3070d169161a563a60f3b3
Nayeem Shiddiki Abir <abircoxsbazar@gmail.com>
2023-08-14 17:36:51 +0600

    box fit added

--- lib/httpreq/httpreg.dart
+++ lib/httpreq/httpreg.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.blue),
+      debugShowCheckedModeBanner: fa,
       darkTheme: ThemeData(primaryColor: Colors.grey),
       home: const MyScaffoldWidgets(),
     );
